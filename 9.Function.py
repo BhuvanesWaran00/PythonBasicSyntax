952d5a4d588aeabce06cf735e4b681d4bbbcaafe
BhuvanesWaran00 <117109051+BhuvanesWaran00@users.noreply.github.com>
2024-10-24 13:13:06 +0530

    Update 9.Function.py

--- 9.Function.py
+++ 9.Function.py
@@ -19,3 +19,36 @@ def sub(a,b):
 x=1
 y=8
 sub(x,y)
+
+
+
+# Pass multiple types if args
+
+# pass single value via one argument
+def num(numbers):
+    print(numbers)
+
+num(1)
+
+# Pass multiple values via one argument
+def num(numbers): 
+    print(numbers)
+
+try:
+    num(1,2,3,4)
+except:
+    print("can't pass multiple values")
+
+# pass multiple values via one argument using *args
+
+def num(*numbers):
+    print(numbers)
+
+num(1,2,3,4)
+
+# pass key value via one argument using **kwargs
+def num(**numbers):
+    print(numbers)
+
+num(name ="zeo" , age =24)
+
